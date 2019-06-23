194173c86a357a2b1d8c56dcf255b4e8b00386b7
femi <femi@thepoint.africa>
2019-06-23 16:53:14 +0100

    Added test case for GET /

--- README.md
+++ README.md
@@ -2,4 +2,9 @@
 
 A basic authentication system built with Express. It stores users in a Mongo database and generates JWT tokens upon login.
 
-Integration tests writtend with `Mocha and Chai`.
+### Technologies
+ 1. Node.js
+ 1. Express.js
+ 1. Mocha
+ 1. Chai/chai-http
+ 1. MongoDB
--- test/test.js
+++ test/test.js
@@ -1,13 +1,27 @@
 let chai = require('chai');
 const expect = chai.expect;
+const http = require('chai-http');
+chai.use(http);
 
 const app = require('../app');
 
 
 describe('App', () => {
 
-    it('Should exists', () => {
+    it('Should exist', () => {
         expect(app).to.be.a('function');
+    });
+
+    it('GET / should return 200 and message', (done) => {
+        //send a request to the app
+        chai.request(app).get('/')
+            .then((res) => {
+                expect(res).to.have.status(200);
+                expect(res.body.message).to.contain('Yeezys');
+                done();
+            }).catch(err => {
+                console.log(err.message);
+            });
     });
 
 });
