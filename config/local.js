4ea56e2254ec3526a45d8ea9a5e32cf3c4dca647
femi <femicodes@gmail.com>
2019-07-06 08:09:53 +0100

    add travis ci

---
+++ config/local.js
@@ -0,0 +1,7 @@
+module.exports = {
+    APP_PORT: "1337",
+    DB_HOST: "YOUR_MONGO_HOST/DATABASE",
+    DB_USER: "MONGO_USER",
+    DB_PASS: "MONGO_PASS",
+    JWT_KEY: "thisIsMyJwtKeyUsedToEncodeTheTokens"
+}
